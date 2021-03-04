10daa610eeac126d18b99c3bb0828e66bb25d922
khaledgabr77 <khaledgabr77@gmail.com>
2021-03-04 13:33:48 +0200

    add materials

--- vision-hw1/src/resize_image.c
+++ vision-hw1/src/resize_image.c
@@ -1,27 +1,22 @@
-#include <math.h>
 #include "image.h"
+#include <math.h>
 
-float nn_interpolate(image im, float x, float y, int c)
-{
-    // TODO Fill in
-    return 0;
+float nn_interpolate(image im, float x, float y, int c) {
+  // TODO Fill in
+
 }
 
-image nn_resize(image im, int w, int h)
-{
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+image nn_resize(image im, int w, int h) {
+  // TODO Fill in (also fix that first line)
+
 }
 
-float bilinear_interpolate(image im, float x, float y, int c)
-{
-    // TODO
-    return 0;
+float bilinear_interpolate(image im, float x, float y, int c) {
+  // TODO
+  return 0;
 }
 
-image bilinear_resize(image im, int w, int h)
-{
-    // TODO
-    return make_image(1,1,1);
+image bilinear_resize(image im, int w, int h) {
+  // TODO
+  return make_image(1, 1, 1);
 }
-
