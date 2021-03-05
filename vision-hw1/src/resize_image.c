a97991c797b9d747e1bc6b148a81e4f4814d127b
khaledgabr77 <khaledgabr77@gmail.com>
2021-03-05 14:36:27 +0200

    biliner

--- vision-hw1/src/resize_image.c
+++ vision-hw1/src/resize_image.c
@@ -24,12 +24,39 @@ image nn_resize(image im, int w, int h) {
 }
 
 float bilinear_interpolate(image im, float x, float y, int c) {
-  // TODO
-
-  return 0;
+  int left = floorf(x);
+  int right = ceilf(x);
+  int top = floorf(y);
+  int bottom = ceilf(y);
+  float v1 = get_pixel(im, left, top, c);
+  float v2 = get_pixel(im, right, top, c);
+  float v3 = get_pixel(im, left, bottom, c);
+  float v4 = get_pixel(im, right, bottom, c);
+  float d1 = bottom - y;
+  float d2 = y - top;
+  float d3 = x - left;
+  float d4 = right - x;
+  float q1 = v1 * d1 + v3 * d2;
+  float q2 = v2 * d1 + v4 * d2;
+  float q = q2 * d3 + q1 * d4;
+  return q;
 }
 
 image bilinear_resize(image im, int w, int h) {
-  // TODO
-  return make_image(1, 1, 1);
+  image ret = make_image(w, h, im.c);
+  float a_x = (float)im.w / w;
+  float a_y = (float)im.h / h;
+  float b_x = -0.5 + 0.5 * a_x;
+  float b_y = -0.5 + 0.5 * a_y;
+  for (int x = 0; x < w; x++) {
+    for (int y = 0; y < h; y++) {
+      for (int c = 0; c < im.c; c++) {
+        float x_new = a_x * x + b_x;
+        float y_new = a_y * y + b_y;
+        ret.data[x + y * w + c * w * h] =
+            bilinear_interpolate(im, x_new, y_new, c);
+      }
+    }
+  }
+  return ret;
 }
